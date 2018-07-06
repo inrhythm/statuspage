93af715753f9d9270cfc138d1d18b8c0cb65d141
jimitpatel65 <jimitpatel.65@gmail.com>
2018-07-06 14:58:51 -0400

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # statuspage
 Application providing uptime information on a variety of IR's services and dependencies
+
+# Steps to run locally
+```git clone https://github.com/inrhythm/statuspage.git
+cd statuspage
+npm install
+npm start```
--- src/Components/OverAllStatus/OverAllStatus.js
+++ src/Components/OverAllStatus/OverAllStatus.js
@@ -3,7 +3,7 @@ import './OverAllStatus.scss';
 
 const OverAllStatus = ({overAllStatus}) => <p
   className={`OverAllStatus-p ${overAllStatus ? 'OverAllStatus-success' : 'OverAllStatus-error'}`}>
-  {overAllStatus ? 'All Systems are Operational' : 'All Systems are not Operational'}
+  {overAllStatus ? 'All IR Services are Operational' : 'Few IR services are down'}
 </p>;
 
 export default OverAllStatus;
